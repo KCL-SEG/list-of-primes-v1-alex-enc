d47749595a7a590e66a24a9fcbaa6f7face08a14
Alexandra Encarnacion <alexandra.encarnacion@kcl.ac.uk>
2022-10-02 16:40:27 +0100

    Add a condition for 0 prime numbers

--- primes.py
+++ primes.py
@@ -9,21 +9,25 @@ def primes(number_of_primes):
     amountOfPrimes = 1
 
     x = 2
-    list.append(2)
-    while amountOfPrimes < number_of_primes:
-        x += 1
-        prime = True
+    if number_of_primes == 0:
+        list = []
+    else:
+
+        list.append(2)
+        while amountOfPrimes < number_of_primes:
+            x += 1
+            prime = True
 
 
-        for y in range(2,x):
-            if (x%y==0):
-                prime = False
-        if prime:
-            if x in freq:
-                freq[x] +=1
-            else:
-                amountOfPrimes += 1
-                list.append(x)
-                freq[x] = 1
+            for y in range(2,x):
+                if (x%y==0):
+                    prime = False
+            if prime:
+                if x in freq:
+                    freq[x] +=1
+                else:
+                    amountOfPrimes += 1
+                    list.append(x)
+                    freq[x] = 1
 
     return list
