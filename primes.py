ba566960e8bbab1410c3953ce3aa59e0dd427755
Alexandra Encarnacion <alexandra.encarnacion@kcl.ac.uk>
2022-10-02 16:25:59 +0100

    Obtain the correct amount of prime numbers

--- primes.py
+++ primes.py
@@ -1,15 +1,29 @@
+
+
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+    freq= {}
+    amountOfPrimes = 1
 
-    for x in range(2,number_of_primes + 2):
+    x = 2
+    list.append(2)
+    while amountOfPrimes < number_of_primes:
+        x += 1
         prime = True
+
+
         for y in range(2,x):
             if (x%y==0):
                 prime = False
         if prime:
-            list.append(x)
+            if x in freq:
+                freq[x] +=1
+            else:
+                amountOfPrimes += 1
+                list.append(x)
+                freq[x] = 1
 
     return list
