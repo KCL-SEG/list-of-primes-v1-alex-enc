25ea3be5c06bec8e91fdec916f537fd753c0f587
Alexandra Encarnacion <alexandra.encarnacion@kcl.ac.uk>
2022-10-02 15:35:09 +0100

    Find a certain amount of prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+
+    for x in range(2,number_of_primes + 2):
+        prime = True
+        for y in range(2,x):
+            if (x%y==0):
+                prime = False
+        if prime:
+            list.append(x)
+
     return list
